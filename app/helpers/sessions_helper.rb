807ce898a586e2ab6c1670ae4fcb620cb1d98c94
github username <github email address>
2018-12-19 14:24:46 -0800

    Switch redirect path to root url after login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,7 +17,7 @@ module SessionsHelper
   def log_user_in
     session[:user_id] = @user.id
     flash[:success] = "Logged in succesfully"
-    redirect_to feed_path
+    redirect_to '/'
   end
 
 end
