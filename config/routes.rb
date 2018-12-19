1331e769919dab4038b1117f9736c6676d51b73e
github username <github email address>
2018-12-19 14:23:19 -0800

    Draw root url to sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'sessions#new'
+
   get '/hashtags', to: 'tags#index', as: :hashtags
   get '/hashtags/:hashtag', to: 'tags#show', as: :hashtag
 
   get '/boards/feed' => 'boards#feed', as: :feed
+
 
   get '/sortedtags' => 'tags#sorted_tags'
 
