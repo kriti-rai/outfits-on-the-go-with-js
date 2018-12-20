7a8ee10c1f3c647f96c83f1eeab264133e88f4a0
github username <github email address>
2018-12-20 00:19:12 -0800

    Add attachListeners()

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,17 +2,14 @@ $(document).on('turbolinks:load', function() {
   attachListeners();
 })
 
-var showUser = (id) => {
-  // shows a user's profile
-  //includes next button that takes to another user's page
-  // $.get(`/users/${id}`, function(data) {
-  //
-  // }
-}
-
 
 function attachListeners() {
-  $('.user-show').on('click', function() {
-    debugger
-  })
-}
+  $('.user-show').on('click', function(e) {
+    $.ajax({
+      url: this.href,
+      dataType: 'script'
+    })
+
+    e.preventDefault();
+  });
+};
