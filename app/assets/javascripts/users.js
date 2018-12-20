7540d18a17ebcff9e925d88fd47d9c8a8389c442
github username <github email address>
2018-12-20 14:46:50 -0800

    Create showUser()

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,14 +2,32 @@ $(document).on('turbolinks:load', function() {
   attachListeners();
 })
 
+var showUser = (user) => {
+  $('.users').empty();
+  $.get(`${user.href}`, function(user) {
+    var noImagePath = "./images/no_image.png"
+    if (user.image != null) {
+      $('.users').prepend($('<img>', {class:'user-show', src:`${user.image.url}`}))
+    } else {
+      $('.users').prepend($('<img>', {class:'user-show', src:'../../no_image.png'})) //image path does not work
+
+    }
+    $('.users').append($(`<br><h1>${user.username}!</h1>`))
+    user.image != null ? $('.users').append(`<h6><em><font color= 'grey'>Bio: ${user.bio}</em></h6>`) : false
+    if (user.boards.length) {
+      $('.users').append(`<p class="button" src= /users/${user.id}/boards}>Boards</p></br>`)
+    }
+
+
+    $('.users').append(`<p class="button">Next User</p>`)
+
+  })
+
+}
 
 function attachListeners() {
   $('.user-show').on('click', function(e) {
-    $.ajax({
-      url: this.href,
-      dataType: 'script'
-    })
-
+    showUser(this)
     e.preventDefault();
   });
 };
