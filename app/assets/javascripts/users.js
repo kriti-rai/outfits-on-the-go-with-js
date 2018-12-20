4499734975a6114570bc9346b300bc389f568224
github username <github email address>
2018-12-19 16:28:11 -0800

    Create js files for boards, users and outfits

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,18 @@
+$(document).on('turbolinks:load', function() {
+  attachListeners();
+})
+
+var showUser = (id) => {
+  // shows a user's profile
+  //includes next button that takes to another user's page
+  // $.get(`/users/${id}`, function(data) {
+  //
+  // }
+}
+
+
+function attachListeners() {
+  $('.user-show').on('click', function() {
+    debugger
+  })
+}
