fbf7afd52670ed3631ed090a9df0ca76b0a84992
github username <github email address>
2018-12-22 16:34:18 -0800

    Add showLoggedUserHTML and button to take to user's boards

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,72 @@
 $(document).on('turbolinks:load', function() {
   attachListeners();
-})
+});
+
+function attachListeners() {
+  //shows individual profile
+  $('.user-show').on('click', function(e) {
+    showUser(this);
+    e.preventDefault();
+  });
+
+  //upon login shows to user's profile
+  $('.login-form').on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: this.action,
+      method: 'POST',
+      data:  {
+        'authenticity_token': $("input[name='authenticity_token']").val(),
+        'username': $("input[name='username']").val(),
+        'password': $("input[name='password']").val()
+      }
+    }).done(function (user) {
+      e.preventDefault()
+      $('.col-lg-12').empty();
+      $.get(`/users/${user.id}`, function(user) {
+        showLoggedUserHTML(user);
+      });
+    });
+  });
+
+  $('body').on('click', '#boards', function (e) {
+    $.get(this.value, function(boards) {
+      e.preventDefault();
+      $('.users').empty();
+      $('.users').append('<h1>Boards</h1>')
+      boards.forEach(function(board) {
+        $('.users').append(`<h5><a href="/boards/${board.id}">${board.name}</a></h5>`)
+      });
+    });
+   });
+
+   // $('body').on('click', '#nextUser', function () {
+   //    alert("Next");
+   //  });
+};
+
+// var boardsButton = (user) => {
+//   $.get(`/users/${user.id}/boards`, function(boards) {
+//     debugger
+//   });
+// };
+
+var showLoggedUserHTML = (user) => {
+  var noImagePath = "./images/no_image.png"
+  if (user.image != null) {
+    $('.col-lg-12').prepend($('<img>', {class:'user-show', src:`${user.image.url}`}))
+  } else {
+    $('.col-lg-12').prepend($('<img>', {class:'user-show', src:'../../no_image.png'})) //image path does not work
+
+  }
+  $('.col-lg-12').append($(`<br><h1>${user.username}!</h1>`))
+  user.image != null ? $('.col-lg-12').append(`<h6><em><font color= 'grey'>Bio: ${user.bio}</em></h6>`) : false
+  if (user.boards.length) {
+    $('.col-lg-12').append(`<p><button value="/users/${user.id}/boards" class="button">Boards</button><p>`)
+  }
+
+  $('.col-lg-12').append(`<p><button id="nextUser" class="button">Next User</button></p>`)
+}
 
 var showUser = (user) => {
   $('.users').empty();
@@ -15,19 +81,18 @@ var showUser = (user) => {
     $('.users').append($(`<br><h1>${user.username}!</h1>`))
     user.image != null ? $('.users').append(`<h6><em><font color= 'grey'>Bio: ${user.bio}</em></h6>`) : false
     if (user.boards.length) {
-      $('.users').append(`<p class="button" src= /users/${user.id}/boards}>Boards</p></br>`)
+      $('.users').append(`<p><button value="/users/${user.id}/boards" class="button" id="boards">Boards</button></p>`)
     }
 
 
-    $('.users').append(`<p class="button">Next User</p>`)
-
+    $('.users').append(`<p><button id="nextUser" class="button">Next User</button></p>`)
   })
-
 }
 
-function attachListeners() {
-  $('.user-show').on('click', function(e) {
-    showUser(this)
-    e.preventDefault();
-  });
-};
+// var showNextUser = () => {
+//   $('#boards.button').on('click', function(e) {
+//     // e.preventDefault();
+//     alert('hey')
+//     // debugger
+//   })
+// }
