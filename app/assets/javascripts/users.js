e9ae6cb7ee062587f9c6fbd1de122fa0c9f994db
github username <github email address>
2018-12-22 21:12:31 -0800

    Format next user button + refactor

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -29,9 +29,10 @@ function attachListeners() {
     });
   });
 
+  //Boards button
   $('body').on('click', '#boards', function (e) {
+    e.preventDefault();
     $.get(this.value, function(boards) {
-      e.preventDefault();
       $('.users').empty();
       $('.users').append('<h1>Boards</h1>')
       boards.forEach(function(board) {
@@ -40,17 +41,35 @@ function attachListeners() {
     });
    });
 
-   // $('body').on('click', '#nextUser', function () {
-   //    alert("Next");
-   //  });
+   //Next User button
+   $('body').on('click', '#nextUser', function (e) {
+     e.preventDefault();
+     $.get(this.value, function(user) {
+         $('.users').empty()
+         userHTML(user)
+     });
+  });
 };
 
-// var boardsButton = (user) => {
-//   $.get(`/users/${user.id}/boards`, function(boards) {
-//     debugger
-//   });
-// };
+var userHTML = (user) => {
+  var noImagePath = "./images/no_image.png"
+  if (user.image != null) {
+    $('.users').prepend($('<img>', {class:'user-show', src:`${user.image.url}`}))
+  } else {
+    $('.users').prepend($('<img>', {class:'user-show', src:'../../no_image.png'})) //image path does not work
 
+  }
+  $('.users').append($(`<br><h1>${user.username}!</h1>`))
+
+  user.image != null ? $('.users').append(`<h6><em><font color= 'grey'>Bio: ${user.bio}</em></h6>`) : false
+
+  if (user.boards.length) {
+    $('.users').append(`<p><button value="/users/${user.id}/boards" class="button" id="boards">Boards</button></p>`)
+  };
+
+  $('.users').append(`<p><button value="/users/${user.id}/next" id="nextUser" class="button">Next User</button></p>`)
+
+}
 var showLoggedUserHTML = (user) => {
   var noImagePath = "./images/no_image.png"
   if (user.image != null) {
@@ -65,34 +84,12 @@ var showLoggedUserHTML = (user) => {
     $('.col-lg-12').append(`<p><button value="/users/${user.id}/boards" class="button">Boards</button><p>`)
   }
 
-  $('.col-lg-12').append(`<p><button id="nextUser" class="button">Next User</button></p>`)
+  $('.col-lg-12').append(`<p><button value="/users/${user.id}/next" id="nextUser" class="button">Next User</button></p>`)
 }
 
 var showUser = (user) => {
   $('.users').empty();
   $.get(`${user.href}`, function(user) {
-    var noImagePath = "./images/no_image.png"
-    if (user.image != null) {
-      $('.users').prepend($('<img>', {class:'user-show', src:`${user.image.url}`}))
-    } else {
-      $('.users').prepend($('<img>', {class:'user-show', src:'../../no_image.png'})) //image path does not work
-
-    }
-    $('.users').append($(`<br><h1>${user.username}!</h1>`))
-    user.image != null ? $('.users').append(`<h6><em><font color= 'grey'>Bio: ${user.bio}</em></h6>`) : false
-    if (user.boards.length) {
-      $('.users').append(`<p><button value="/users/${user.id}/boards" class="button" id="boards">Boards</button></p>`)
-    }
-
-
-    $('.users').append(`<p><button id="nextUser" class="button">Next User</button></p>`)
+    userHTML(user);
   })
 }
-
-// var showNextUser = () => {
-//   $('#boards.button').on('click', function(e) {
-//     // e.preventDefault();
-//     alert('hey')
-//     // debugger
-//   })
-// }
