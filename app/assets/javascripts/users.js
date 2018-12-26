d7cae895b60fefe8b7198e82ae5ff5c9f8a670f7
github username <github email address>
2018-12-25 23:20:52 -0800

    Change selectors, refactor and comment out logged user functionalities

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,81 +3,106 @@ $(document).on('turbolinks:load', function() {
 });
 
 function attachListenersForUsers() {
-  //shows individual profile
-  $('.user-show').on('click', function(e) {
-    showUser(this);
+  //show users
+  $('#users').on('click', function(e) {
     e.preventDefault();
-  });
-
-  //upon login shows to user's profile
-  $('.login-form').on('submit', function(e) {
-    e.preventDefault();
-    $.ajax({
-      url: this.action,
-      method: 'POST',
-      data:  {
-        'authenticity_token': $("input[name='authenticity_token']").val(),
-        'username': $("input[name='username']").val(),
-        'password': $("input[name='password']").val()
-      }
-    }).done(function (user) {
-      e.preventDefault()
-      $('.col-lg-12').empty();
-      $.get(`/users/${user.id}`, function(user) {
-        showLoggedUserHTML(user);
-      });
+    $.get(`${this.href}`, function(users) {
+      listUsers(users);
     });
   });
 
-   //Next User button
-   $('body').on('click', '#nextUser', function (e) {
-     e.preventDefault();
-     $.get(this.value, function(user) {
-         $('.users').empty()
-         userHTML(user)
-     });
+  //Next User button
+  $('body').on('click', '#nextUser', function (e) {
+    e.preventDefault();
+    $('.col-lg-12').empty()
+    $.get(this.dataset.url, function(user) {
+      userHTML(user)
+    });
+ });
+
+  //upon login shows to user's profile
+  // $('.login-form').on('submit', function(e) {
+  //   debugger
+  //   e.preventDefault();
+  //   $.ajax({
+  //     url: this.action,
+  //     method: 'POST',
+  //     data:  {
+  //       'authenticity_token': $("input[name='authenticity_token']").val(),
+  //       'username': $("input[name='username']").val(),
+  //       'password': $("input[name='password']").val()
+  //     }
+  //   }).done(function (user) {
+  //     e.preventDefault()
+  //     $('.col-lg-12').empty();
+  //     $('.col-lg-12').prepend("<br><br><br><br><br>")
+  //     $.get(`/users/${user.id}`, function(user) {
+  //       showLoggedUserHTML(user);
+  //     });
+  //   });
+  // });
+
+
+
+};
+
+var listUsers = (users) => {
+  $('.col-lg-12').empty()
+  users.forEach(function(user) {
+    $('.col-lg-12').append("<br><br>")
+    if (user.image.url === null) {
+      $('.col-lg-12').append(`<input type='image' class='user-thumbnail', src="/assets/no_image.png", data-id='${user.id}', onclick='showUser(this)'></input>`)
+    } else {
+      var img = new Image();
+      $('.col-lg-12').append(`<input type='image' class='user-thumbnail', src='${user.image.url}', data-id='${user.id}', onclick='showUser(this)'></input>`)
+    }
+    $('.col-lg-12').append($(`<div class="figcaption">${user.username}</div>`))
   });
 };
 
 var userHTML = (user) => {
-  var noImagePath = "./images/no_image.png"
-  if (user.image != null) {
-    $('.users').prepend($('<img>', {class:'user-show', src:`${user.image.url}`}))
+  $('.col-lg-12').append("<br><br>")
+  if (user.image.url != null) {
+    $('.col-lg-12').append($('<img>', {class:'user-show', src:`${user.image.url}`}))
   } else {
-    $('.users').prepend($('<img>', {class:'user-show', src:'../../no_image.png'})) //image path does not work
-
-  }
-  $('.users').append($(`<br><h1>${user.username}!</h1>`))
-
-  user.image != null ? $('.users').append(`<h6><em><font color= 'grey'>Bio: ${user.bio}</em></h6>`) : false
-
-  if (user.boards.length) {
-    $('.users').append(`<p><button value="/users/${user.id}/boards" class="button" id="boards">Boards</button></p>`)
-  };
-
-  $('.users').append(`<p><button value="/users/${user.id}/next" id="nextUser" class="button">Next User</button></p>`)
-
-}
-var showLoggedUserHTML = (user) => {
-  var noImagePath = "./images/no_image.png"
-  if (user.image != null) {
-    $('.col-lg-12').prepend($('<img>', {class:'user-show', src:`${user.image.url}`}))
-  } else {
-    $('.col-lg-12').prepend($('<img>', {class:'user-show', src:'../../no_image.png'})) //image path does not work
-
+    $('.col-lg-12').append($('<img>', {class:'user-show', src:"/assets/no_image.png"}))
   }
   $('.col-lg-12').append($(`<br><h1>${user.username}!</h1>`))
-  user.image != null ? $('.col-lg-12').append(`<h6><em><font color= 'grey'>Bio: ${user.bio}</em></h6>`) : false
-  if (user.boards.length) {
-    $('.col-lg-12').append(`<p><button value="/users/${user.id}/boards" class="button">Boards</button><p>`)
-  }
 
-  $('.col-lg-12').append(`<p><button value="/users/${user.id}/next" id="nextUser" class="button">Next User</button></p>`)
+  user.image != null ? $('.col-lg-12').append(`<h6><em><font color= 'grey'>Bio: ${user.bio}</em></h6>`) : false
+
+  if (user.boards.length) {
+    $('.col-lg-12').append(`<p><button data-url="/users/${user.id}/boards" class="button" id="boards">Boards</button></p>`)
+  };
+
+  $('.col-lg-12').append(`<p><button data-url="/users/${user.id}/next" id="nextUser" class="button">Next User</button></p>`)
+
 }
 
 var showUser = (user) => {
-  $('.users').empty();
-  $.get(`${user.href}`, function(user) {
+  var url= `/users/${user.dataset.id}`
+  $.getJSON(url, function(user) {
+    $('.col-lg-12').empty();
     userHTML(user);
-  })
-}
+  });
+};
+
+// var feed = () => {
+//
+// }
+// var showLoggedUserHTML = (user) => {
+//   var noImagePath = "./images/no_image.png"
+//   if (user.image != null) {
+//     $('.col-lg-12').append($('<img>', {class:'user-show', src:`${user.image.url}`}))
+//   } else {
+//     $('.col-lg-12').append($('<img>', {class:'user-show', src:'../../no_image.png'})) //image path does not work
+//
+//   }
+//   $('.col-lg-12').append($(`<br><h1>${user.username}!</h1>`))
+//   user.image != null ? $('.col-lg-12').append(`<h6><em><font color= 'grey'>Bio: ${user.bio}</em></h6>`) : false
+//   if (user.boards.length) {
+//     $('.col-lg-12').append(`<p><button data-url="/users/${user.id}/boards" class="button">Boards</button><p>`)
+//   }
+//
+//   $('.col-lg-12').append(`<p><button data-url="/users/${user.id}/next" id="nextUser" class="button">Next User</button></p>`)
+// }
