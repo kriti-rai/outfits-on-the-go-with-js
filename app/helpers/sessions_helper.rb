3b4b730d36c563cdd6acf6b54f6e79afce10380b
github username <github email address>
2018-12-26 23:35:06 -0800

    Switch back to redirect_to @user after login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,7 +17,7 @@ module SessionsHelper
   def log_user_in
     session[:user_id] = @user.id
     flash[:success] = "Logged in succesfully"
-    render json: @user
+    redirect_to @user
   end
 
 end
