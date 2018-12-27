faa3458e1dc66793e7b4c0a152f1272d5627dbcc
github username <github email address>
2018-12-26 23:18:56 -0800

    Render json

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,12 +2,14 @@ class TagsController < ApplicationController
 
   def index
     @hashtags = Tag.all.sort_by{|tag| tag.name}
+    render json: @hashtags
   end
 
   def show
     if tag = Tag.find_by(name: params[:hashtag])
       @outfits = tag.outfits
     end
+    render json: @outfits
   end
 
   def sorted_tags
