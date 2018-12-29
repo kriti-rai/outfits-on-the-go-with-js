4c91d9aa0a56d6fea17ce4d58a3797a27af1ad50
github username <github email address>
2018-12-28 20:44:39 -0800

    Added created_at attribute

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,5 +1,5 @@
 class BoardSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :created_at
   belongs_to :user, serializer: BoardUserSerializer
   has_many :outfits
 end
