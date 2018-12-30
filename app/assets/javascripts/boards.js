05a11a1b1f81ff42dbbdad222a946d91eee13bfc
github username <github email address>
2018-12-30 12:44:56 -0800

    Remove listOutifts(), createOutfitThumbnail(), showOutfit(), listTaggedOutfits()

--- app/assets/javascripts/boards.js
+++ app/assets/javascripts/boards.js
@@ -54,57 +54,7 @@ function clear() {
   $('.col-lg-12').empty();
 };
 
-var listOutfits = (data) => {
-  var url = data.href + "/outfits"
-  clear();
-  $.get(url, function(outfits) {
-    if (outfits.length) {
-      $('.col-lg-12').append('<h1>Outfits</h1>')
-      outfits.forEach(function(outfit) {
-        createOutfitThumbnail(outfit);
-      });
-    } else {
-      $('.col-lg-12').append('<h1>This board has no outfits</h1>')
-    };
-  });
-};
 
-var createOutfitThumbnail = (outfit) => {
-  $('.col-lg-12').append(`<input type='image' class='outfit-thumbnail', src='${outfit.image.url}', data-id='${outfit.id}', onclick='showOutfit(this)'></input>`)
-  if (outfit.caption != null) {
-    $('.col-lg-12').append(`<p><font color="grey"><em>${outfit.caption}</em></font></p>`)
-  };
-};
-
-var showOutfit = (outfit) => {
-  clear();
-  var url = `/outfits/${outfit.dataset.id}`
-  $.get(url, function (outfit) {
-    $('.col-lg-12').append($('<img>', {class:'outfit-show', src:`${outfit.image.url}`}))
-    if (outfit.hashtags) {
-      var tagsLabel = "<p>Tags: "
-      var tags = []
-      outfit.tags.forEach(function(tag) {
-        tags.push(`#<a href='#' class="tags" data-id= "${tag.id}" data-name="${tag.name}">${tag.name}</a>`)
-      });
-      var tagsHTML = tags.join(' ')
-      var innerHTML = tagsLabel + tagsHTML + "</p>"
-      $('.col-lg-12').append(innerHTML)
-    };
-  });
-};
-
-var listTaggedOutfits = (tag) => {
-  clear();
-  $.get(`/hashtags/${tag.dataset.name}`, function (outfits) {
-    if (outfits.length) {
-      $('.col-lg-12').append(`<h1>#${tag.dataset.name}</h1>`)
-      outfits.forEach(function(outfit) {
-        createOutfitThumbnail(outfit);
-      });
-    };
-  });
-};
 
 function formatDate(date) {
   const d = new Date(date)
