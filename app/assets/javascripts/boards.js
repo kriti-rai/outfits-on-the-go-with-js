2cd7884bbec0bfd1d0746e589031a74e14b96338
github username <github email address>
2018-12-30 19:19:42 -0800

    Update listBoards() and Board.prototype.createBoardLinks() to use currentUID

--- app/assets/javascripts/boards.js
+++ app/assets/javascripts/boards.js
@@ -14,16 +14,24 @@ class Board {
   }
 
   createBoardLinks() {
-    let innerHTML = `
-      <br>
-      <h4>
+    let innerHTML = "<br><h4>"
+    let currentUserBoardsHTML = `
         <a href="/boards/${this.id}" class="board">${this.name}</a>
-        <button type="button" class="btn btn-info btn-sm">View</button>
-        <button type="button" class="btn btn-primary btn-sm">Edit</button>
-        <button type="button" class="btn btn-danger btn-sm">Delete</button>
+        <button type="button" data-url="/boards/${this.id}" id="view-board" class="btn btn-info btn-sm">View</button>
+        <button type="button" data-url="/boards/${this.id}/edit" id="edit-board" class="btn btn-primary btn-sm">Edit</button>
+        <button type="button" data-url="/boards/${this.id}" id="delete-board" class="btn btn-danger btn-sm">Delete</button>
       </h4>
       `
-    return innerHTML;
+    let userBoardsHTML = `
+        <a href="/boards/${this.id}" class="board">${this.name}</a>
+        <button type="button" data-url="/boards/${this.id}" id="view-board" class="btn btn-info btn-sm">View</button>
+      </h4>
+      `
+    if (this.id === currentUID) {
+      return innerHTML + currentUserBoardsHTML
+    } else {
+    return innerHTML + userBoardsHTML;
+    }
   };
 }
 
@@ -35,23 +43,39 @@ function attachListenersForBoards() {
      listBoards(url);
     });
 
-   //list outfits
-   $('body').on('click', 'a.board', function (e) {
+   //render create form
+   $('body').on('click', '#create-board', function (e) {
+     debugger
      e.preventDefault();
-     listOutfits(this);
-    });
+     $.get(this.href, function(form) {
+       clear();
+       $('.col-lg-12').append(form)
+     })
+   });
 
-    //list outfits under that hashtag
-    $('body').on('click', 'a.tags', function (e) {
-      e.preventDefault();
-      listTaggedOutfits(this);
+   //create board
+   $('body').on('submit', '#create-board', function (e) {
+     e.preventDefault();
+     $.ajax({
+       url: this.action,
+       type: "POST",
+       data: $(this).serialize(),
+       success: function(response) {
+         listBoards(response);
+       }
      });
+    });
 };
 
 var listBoards = (url) => {
   $.get(url, function(boards) {
     clear();
-    $('.col-lg-12').append('<h1>Boards <button type="button" class="btn btn-success">Create Board</button></h1>')
+    let user = boards[0].user
+    if (user.id === currentUID) {
+      $('.col-lg-12').append(`<h1> Your Boards <button type="button" data-url="/users/${currentUID}/boards/new" id="create-board" class="btn btn-success">Create Board</button></h1>`)
+    } else {
+      $('.col-lg-12').append(`<h1>${user.username}'s Boards</h1>`)
+    }
     boards.forEach(function(board) {
       let newBoard = new Board(board);
       $('.col-lg-12').append(newBoard.createBoardLinks());
@@ -59,8 +83,23 @@ var listBoards = (url) => {
   });
 };
 
-var addBoard = (board) => {
+var showBoard = (uid, id) => {
 
+}
+
+// var listBoardsForCurrentUser  = (url) => {
+//   $.get(url, function(boards) {
+//     clear();
+//     $('.col-lg-12').append(`<h1>Boards <button type="button" data-url="#" id="create-board" class="btn btn-success">Create Board</button></h1>`)
+//     boards.forEach(function(board) {
+//       let newBoard = new Board(board);
+//       $('.col-lg-12').append(newBoard.createBoardLinks());
+//     });
+//   });
+// };
+
+var createBoard = (board) => {
+  debugger
 };
 
 var deleteBoard = (board) => {
