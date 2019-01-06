cbeb516c8bc5481c9ca763829419967a640ca7fd
github username <github email address>
2019-01-06 15:45:46 -0800

    Update viewBoard()

--- app/assets/javascripts/boards.js
+++ app/assets/javascripts/boards.js
@@ -99,12 +99,11 @@ function attachListenersForBoards() {
 function viewBoard (url) {
   clear();
   $.get(url, function(board) {
-    $('.col-lg-12').append(`<h1>${board.name}`);
+    $('.col-lg-12').prepend(`<span class="display-2">${board.name}</span><br><br><button type="button" data-url="/users/${board.user.id}/boards" id="back-boards" onclick="listBoards(${board.user.id}, this.dataset.url)" class="btn btn-outline-secondary">Back</button> `);
     let user = board.user;
     if (user.id === currentUID) {
-      $('.col-lg-12').append(`<button type="button" data-url="/boards/${board.id}/outfits/new" id="create-outfit" class="btn btn-outline-success">Add Outfit</button></h1> | `)
+      $('.col-lg-12').append(`<button type="button" data-url="/boards/${board.id}/outfits/new" id="create-outfit" class="btn btn-outline-secondary">Add Outfit</button></span> `)
     }
-    $('.col-lg-12').append(`<button type="button" data-url="/users/boards/${board.id}" id="back-boards" class="btn btn-outline-success">Back</button></h1>`)
    listOutfits(board.outfits);
    });
 }
