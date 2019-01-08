efd074f0989dc013add39a79cb6c8f80bb3d5876
github username <github email address>
2019-01-07 21:02:01 -0800

    Change to created_at

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,7 +1,7 @@
 class Board < ApplicationRecord
   belongs_to :user
   has_many :outfits, dependent: :destroy
-  scope :newest_to_oldest, -> {order("boards.updated_at DESC")}
+  scope :newest_to_oldest, -> {order("boards.created_at DESC")}
 
   def created_date
     self.created_at.strftime("%b %e, %l:%M %p")
